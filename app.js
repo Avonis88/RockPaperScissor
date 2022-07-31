4ecb5fbbe2ca2e871d4abcd49031269702020a03
Florian Leonhardt <Florian.Leonhardt@gmail.com>
2022-07-31 14:46:06 +0200

    added playround against computer

--- app.js
+++ app.js
@@ -2,13 +2,13 @@
 
 function getComputerChoice() {
   const randomNumber = Math.floor(Math.random() * 3);
-  /*   if (randomNumber === 0) {
+ /*    if (randomNumber === 0) {
     return "Rock";
   } else if (randomNumber === 1) {
     return "Paper";
   } else randomNumber === 2;
-  return "Scissor"; */
-
+  return "Scissor";
+ */
   switch (randomNumber) {
     case 0:
       return "Rock";
@@ -20,4 +20,26 @@ function getComputerChoice() {
   }
 }
 
-alert(getComputerChoice());
+/* alert(getComputerChoice()); */
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Its a Tie";
+    }
+    if (computerSelection === "Scissor") {
+        return "You Win! Rock beats Scissor"
+    }
+    else {
+        return "You Loose! Paper beats Scissor"
+    }
+}   
+    
+
+    
+    /* Rock > Scissors, Rock < Paper, */
+
+
+
+const playerSelection = "roCk".toUpperCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection))
